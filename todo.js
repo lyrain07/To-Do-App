a3fc26515acf24c58f47b412ceb91f82a88044db
lyrain07 <reemashrestha2019@gmail.com>
2026-01-20 21:23:35 +0545

    JavaScript Update

---
+++ todo.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.querySelector('#task');
+    const submit=document.querySelector('#Submit');
+    const tasklist=document.querySelector('#tasks');
+    submit.disabled = true;
+
+    taskInput.onkeyup = () => {
+        if(taskInput.value.length >0) {
+            submit.disabled = false;
+        }
+        else {
+            submit.disabled = true;
+        }
+    }
+    document.querySelector('form').onsubmit = () => {
+        const task = taskInput.value.trim();
+        const del = document.createElement('button');
+        del.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+        const li = document.createElement('li');
+        li.innerHTML =task;
+        li.append(del);
+        del.onclick = () => {
+            del.parentElement.remove();
+        };
+        tasklist.append(li);
+        taskInput.value='';
+        submit.disabled=true;
+        return false;
+    }
+});
